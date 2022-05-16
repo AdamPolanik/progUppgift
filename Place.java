9ab25277d28af100405b98e0d10ea3e42a327147
ArtinM00 <armo9784@student.SU.SE>
2022-05-16 14:59:59 +0200

    Nu funkar markering av noderna från blå till röd och vice versa

--- Place.java
+++ Place.java
@@ -1,17 +1,39 @@
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
-public class Place extends Canvas {
+public class Place extends Circle {
+
+    private boolean marked = false;
 
     public Place(double x, double y){
-        super(x,y);
-        GraphicsContext gc = getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-        gc.fillOval(getLayoutX(), getLayoutY(), 20, 20);
-        relocate((x - 10), (y - 10));
+        super(x,y, 10, Color.BLUE);
+        setOnMouseClicked(new ClickHandler());
+    }
+
+    class ClickHandler implements EventHandler<MouseEvent>{
+        @Override
+        public void handle(MouseEvent event){
+            if (!marked){
+                rePaintNode();
+                marked = true;
+            } else {
+                paintNode();
+                marked =! marked;
+            }
+        }
+    }
+    
+    public void rePaintNode (){
+        setFill(Color.RED);
+    }
+
+    public void paintNode(){
+        setFill(Color.BLUE);
     }
 
 }
